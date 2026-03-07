7beb4129d26640fa11f757ad7c12131b75d96700
Abraham Perez <156040006+AbeDaDev@users.noreply.github.com>
2026-03-06 19:24:28 -0800

    .

--- Victors_RealEstate/vite.config.js
+++ Victors_RealEstate/vite.config.js
@@ -3,9 +3,10 @@ import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default {
   build: {
     rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'src/index.html') 
-})
+      external: ['some-external-package']
+    }
+  }
+}
