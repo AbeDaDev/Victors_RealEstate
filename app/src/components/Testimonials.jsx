bb4ecba609e8565bc9cca15e4cfb451dd3edf4d9
Abraham Perez <156040006+AbeDaDev@users.noreply.github.com>
2026-03-07 01:38:07 -0800

    .

--- app/src/components/Testimonials.jsx
+++ app/src/components/Testimonials.jsx
@@ -5,10 +5,10 @@ function Testimonials() {
 
       <div className="testimonials-viewport" aria-hidden="false">
         <div className="testimonials-track">
-          <img src="public/testimonial1.png" alt="testimonial 1" className="testimonial" />
-          <img src="public/testimonial2.png" alt="testimonial 2" className="testimonial" />
+          <img src="/testimonial1.png" alt="testimonial 1" className="testimonial" />
+          <img src="/testimonial2.png" alt="testimonial 2" className="testimonial" />
           <img src="public/testimonial1.png" alt="testimonial 1 duplicate" className="testimonial" />
-          <img src="public/testimonial2.png" alt="testimonial 2 duplicate" className="testimonial" />
+          <img src="/testimonial2.png" alt="testimonial 2 duplicate" className="testimonial" />
           {/* duplicates for seamless looping */}
           <img src="public/testimonial1.png" alt="testimonial 1 duplicate" className="testimonial" />
           <img src="public/testimonial2.png" alt="testimonial 2 duplicate" className="testimonial" />
